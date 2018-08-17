b973ce37033a5e578ad7ef9c2375fb51442c73f4
qianxiang <qianxiang@em-data.com.cn>
2018-08-17 11:02:26 +0800

    12

---
+++ app/src/main/java/com/bintutu/shop/bean/LeftBean.java
@@ -0,0 +1,420 @@
+package com.bintutu.shop.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class LeftBean implements Serializable {
+
+
+    /**
+     * 10_FuWeiGao : 0.0
+     * 11_1ZhiZhiGao : 0.0
+     * 12_DaMoZhiGao : 0.0
+     * 13_JiaoWanGao : 0.0
+     * 14_JiaoZhiKuan : 0.0
+     * 15_ZhiWeiKuan : 75.0062
+     * 16_DiBanKuan : 75.0062
+     * 17_MuZhiLiKuan : 9.89464
+     * 18_XiaoZhiWaiKuan : 56.4657
+     * 19_1ZhizhiLiKuan : 17.5741
+     * 1_FootLen : 221.054
+     * 20_5ZhizhiLiKuan : 55.9658
+     * 21_YaoWoWaiKuan : 73.5143
+     * 22_ZhongXinDiKuan : 56.0287
+     * 23_JiaoHuaiNeiKuan : 0.0
+     * 24_MuZhiWaiTuChang : 198.949
+     * 25_XiaoZhiDuanChang : 182.37
+     * 26_XiaoZhiWaiTuChang : 172.422
+     * 27_1ZhiZhiChang : 160.264
+     * 28_5ZhiZhiChang : 140.369
+     * 29_FuGuChang : 121.58
+     * 2_ZhiWei : 0.0
+     * 30_YaoWoChang : 90.6323
+     * 31_ZhouShangDianChang : 85.1059
+     * 32_WaiHuaiGuZhongChang : 49.7372
+     * 33_ZhongXinChang : 39.7898
+     * 34_HouGenBianChang : 8.84217
+     * 35_QianZhangTuDianChang : 152.085
+     * 36_ZuGongDu : 0.0
+     * 37_NeiWaiFanDu : 0.0
+     * 3_FuWei : 0.0
+     * 4_DouWei : 0.0
+     * 5_JiaoWanWei : 0.0
+     * 6_JiaoZhiZhou : 0.0
+     * 7_WaiHuaiXiaGao : 0.0
+     * 8_HouGenTuGao : 0.0
+     * 9_ZhouShangGao : 0.0
+     */
+
+    @SerializedName("10_FuWeiGao")
+    private String _10_FuWeiGao;
+    @SerializedName("11_1ZhiZhiGao")
+    private String _11_1ZhiZhiGao;
+    @SerializedName("12_DaMoZhiGao")
+    private String _12_DaMoZhiGao;
+    @SerializedName("13_JiaoWanGao")
+    private String _13_JiaoWanGao;
+    @SerializedName("14_JiaoZhiKuan")
+    private String _14_JiaoZhiKuan;
+    @SerializedName("15_ZhiWeiKuan")
+    private String _15_ZhiWeiKuan;
+    @SerializedName("16_DiBanKuan")
+    private String _16_DiBanKuan;
+    @SerializedName("17_MuZhiLiKuan")
+    private String _17_MuZhiLiKuan;
+    @SerializedName("18_XiaoZhiWaiKuan")
+    private String _18_XiaoZhiWaiKuan;
+    @SerializedName("19_1ZhizhiLiKuan")
+    private String _19_1ZhizhiLiKuan;
+    @SerializedName("1_FootLen")
+    private String _1_FootLen;
+    @SerializedName("20_5ZhizhiLiKuan")
+    private String _20_5ZhizhiLiKuan;
+    @SerializedName("21_YaoWoWaiKuan")
+    private String _21_YaoWoWaiKuan;
+    @SerializedName("22_ZhongXinDiKuan")
+    private String _22_ZhongXinDiKuan;
+    @SerializedName("23_JiaoHuaiNeiKuan")
+    private String _23_JiaoHuaiNeiKuan;
+    @SerializedName("24_MuZhiWaiTuChang")
+    private String _24_MuZhiWaiTuChang;
+    @SerializedName("25_XiaoZhiDuanChang")
+    private String _25_XiaoZhiDuanChang;
+    @SerializedName("26_XiaoZhiWaiTuChang")
+    private String _26_XiaoZhiWaiTuChang;
+    @SerializedName("27_1ZhiZhiChang")
+    private String _27_1ZhiZhiChang;
+    @SerializedName("28_5ZhiZhiChang")
+    private String _28_5ZhiZhiChang;
+    @SerializedName("29_FuGuChang")
+    private String _29_FuGuChang;
+    @SerializedName("2_ZhiWei")
+    private String _2_ZhiWei;
+    @SerializedName("30_YaoWoChang")
+    private String _30_YaoWoChang;
+    @SerializedName("31_ZhouShangDianChang")
+    private String _31_ZhouShangDianChang;
+    @SerializedName("32_WaiHuaiGuZhongChang")
+    private String _32_WaiHuaiGuZhongChang;
+    @SerializedName("33_ZhongXinChang")
+    private String _33_ZhongXinChang;
+    @SerializedName("34_HouGenBianChang")
+    private String _34_HouGenBianChang;
+    @SerializedName("35_QianZhangTuDianChang")
+    private String _35_QianZhangTuDianChang;
+    @SerializedName("36_ZuGongDu")
+    private String _36_ZuGongDu;
+    @SerializedName("37_NeiWaiFanDu")
+    private String _37_NeiWaiFanDu;
+    @SerializedName("3_FuWei")
+    private String _3_FuWei;
+    @SerializedName("4_DouWei")
+    private String _4_DouWei;
+    @SerializedName("5_JiaoWanWei")
+    private String _5_JiaoWanWei;
+    @SerializedName("6_JiaoZhiZhou")
+    private String _6_JiaoZhiZhou;
+    @SerializedName("7_WaiHuaiXiaGao")
+    private String _7_WaiHuaiXiaGao;
+    @SerializedName("8_HouGenTuGao")
+    private String _8_HouGenTuGao;
+    @SerializedName("9_ZhouShangGao")
+    private String _9_ZhouShangGao;
+
+    public String get_10_FuWeiGao() {
+        return _10_FuWeiGao;
+    }
+
+    public void set_10_FuWeiGao(String _10_FuWeiGao) {
+        this._10_FuWeiGao = _10_FuWeiGao;
+    }
+
+    public String get_11_1ZhiZhiGao() {
+        return _11_1ZhiZhiGao;
+    }
+
+    public void set_11_1ZhiZhiGao(String _11_1ZhiZhiGao) {
+        this._11_1ZhiZhiGao = _11_1ZhiZhiGao;
+    }
+
+    public String get_12_DaMoZhiGao() {
+        return _12_DaMoZhiGao;
+    }
+
+    public void set_12_DaMoZhiGao(String _12_DaMoZhiGao) {
+        this._12_DaMoZhiGao = _12_DaMoZhiGao;
+    }
+
+    public String get_13_JiaoWanGao() {
+        return _13_JiaoWanGao;
+    }
+
+    public void set_13_JiaoWanGao(String _13_JiaoWanGao) {
+        this._13_JiaoWanGao = _13_JiaoWanGao;
+    }
+
+    public String get_14_JiaoZhiKuan() {
+        return _14_JiaoZhiKuan;
+    }
+
+    public void set_14_JiaoZhiKuan(String _14_JiaoZhiKuan) {
+        this._14_JiaoZhiKuan = _14_JiaoZhiKuan;
+    }
+
+    public String get_15_ZhiWeiKuan() {
+        return _15_ZhiWeiKuan;
+    }
+
+    public void set_15_ZhiWeiKuan(String _15_ZhiWeiKuan) {
+        this._15_ZhiWeiKuan = _15_ZhiWeiKuan;
+    }
+
+    public String get_16_DiBanKuan() {
+        return _16_DiBanKuan;
+    }
+
+    public void set_16_DiBanKuan(String _16_DiBanKuan) {
+        this._16_DiBanKuan = _16_DiBanKuan;
+    }
+
+    public String get_17_MuZhiLiKuan() {
+        return _17_MuZhiLiKuan;
+    }
+
+    public void set_17_MuZhiLiKuan(String _17_MuZhiLiKuan) {
+        this._17_MuZhiLiKuan = _17_MuZhiLiKuan;
+    }
+
+    public String get_18_XiaoZhiWaiKuan() {
+        return _18_XiaoZhiWaiKuan;
+    }
+
+    public void set_18_XiaoZhiWaiKuan(String _18_XiaoZhiWaiKuan) {
+        this._18_XiaoZhiWaiKuan = _18_XiaoZhiWaiKuan;
+    }
+
+    public String get_19_1ZhizhiLiKuan() {
+        return _19_1ZhizhiLiKuan;
+    }
+
+    public void set_19_1ZhizhiLiKuan(String _19_1ZhizhiLiKuan) {
+        this._19_1ZhizhiLiKuan = _19_1ZhizhiLiKuan;
+    }
+
+    public String get_1_FootLen() {
+        return _1_FootLen;
+    }
+
+    public void set_1_FootLen(String _1_FootLen) {
+        this._1_FootLen = _1_FootLen;
+    }
+
+    public String get_20_5ZhizhiLiKuan() {
+        return _20_5ZhizhiLiKuan;
+    }
+
+    public void set_20_5ZhizhiLiKuan(String _20_5ZhizhiLiKuan) {
+        this._20_5ZhizhiLiKuan = _20_5ZhizhiLiKuan;
+    }
+
+    public String get_21_YaoWoWaiKuan() {
+        return _21_YaoWoWaiKuan;
+    }
+
+    public void set_21_YaoWoWaiKuan(String _21_YaoWoWaiKuan) {
+        this._21_YaoWoWaiKuan = _21_YaoWoWaiKuan;
+    }
+
+    public String get_22_ZhongXinDiKuan() {
+        return _22_ZhongXinDiKuan;
+    }
+
+    public void set_22_ZhongXinDiKuan(String _22_ZhongXinDiKuan) {
+        this._22_ZhongXinDiKuan = _22_ZhongXinDiKuan;
+    }
+
+    public String get_23_JiaoHuaiNeiKuan() {
+        return _23_JiaoHuaiNeiKuan;
+    }
+
+    public void set_23_JiaoHuaiNeiKuan(String _23_JiaoHuaiNeiKuan) {
+        this._23_JiaoHuaiNeiKuan = _23_JiaoHuaiNeiKuan;
+    }
+
+    public String get_24_MuZhiWaiTuChang() {
+        return _24_MuZhiWaiTuChang;
+    }
+
+    public void set_24_MuZhiWaiTuChang(String _24_MuZhiWaiTuChang) {
+        this._24_MuZhiWaiTuChang = _24_MuZhiWaiTuChang;
+    }
+
+    public String get_25_XiaoZhiDuanChang() {
+        return _25_XiaoZhiDuanChang;
+    }
+
+    public void set_25_XiaoZhiDuanChang(String _25_XiaoZhiDuanChang) {
+        this._25_XiaoZhiDuanChang = _25_XiaoZhiDuanChang;
+    }
+
+    public String get_26_XiaoZhiWaiTuChang() {
+        return _26_XiaoZhiWaiTuChang;
+    }
+
+    public void set_26_XiaoZhiWaiTuChang(String _26_XiaoZhiWaiTuChang) {
+        this._26_XiaoZhiWaiTuChang = _26_XiaoZhiWaiTuChang;
+    }
+
+    public String get_27_1ZhiZhiChang() {
+        return _27_1ZhiZhiChang;
+    }
+
+    public void set_27_1ZhiZhiChang(String _27_1ZhiZhiChang) {
+        this._27_1ZhiZhiChang = _27_1ZhiZhiChang;
+    }
+
+    public String get_28_5ZhiZhiChang() {
+        return _28_5ZhiZhiChang;
+    }
+
+    public void set_28_5ZhiZhiChang(String _28_5ZhiZhiChang) {
+        this._28_5ZhiZhiChang = _28_5ZhiZhiChang;
+    }
+
+    public String get_29_FuGuChang() {
+        return _29_FuGuChang;
+    }
+
+    public void set_29_FuGuChang(String _29_FuGuChang) {
+        this._29_FuGuChang = _29_FuGuChang;
+    }
+
+    public String get_2_ZhiWei() {
+        return _2_ZhiWei;
+    }
+
+    public void set_2_ZhiWei(String _2_ZhiWei) {
+        this._2_ZhiWei = _2_ZhiWei;
+    }
+
+    public String get_30_YaoWoChang() {
+        return _30_YaoWoChang;
+    }
+
+    public void set_30_YaoWoChang(String _30_YaoWoChang) {
+        this._30_YaoWoChang = _30_YaoWoChang;
+    }
+
+    public String get_31_ZhouShangDianChang() {
+        return _31_ZhouShangDianChang;
+    }
+
+    public void set_31_ZhouShangDianChang(String _31_ZhouShangDianChang) {
+        this._31_ZhouShangDianChang = _31_ZhouShangDianChang;
+    }
+
+    public String get_32_WaiHuaiGuZhongChang() {
+        return _32_WaiHuaiGuZhongChang;
+    }
+
+    public void set_32_WaiHuaiGuZhongChang(String _32_WaiHuaiGuZhongChang) {
+        this._32_WaiHuaiGuZhongChang = _32_WaiHuaiGuZhongChang;
+    }
+
+    public String get_33_ZhongXinChang() {
+        return _33_ZhongXinChang;
+    }
+
+    public void set_33_ZhongXinChang(String _33_ZhongXinChang) {
+        this._33_ZhongXinChang = _33_ZhongXinChang;
+    }
+
+    public String get_34_HouGenBianChang() {
+        return _34_HouGenBianChang;
+    }
+
+    public void set_34_HouGenBianChang(String _34_HouGenBianChang) {
+        this._34_HouGenBianChang = _34_HouGenBianChang;
+    }
+
+    public String get_35_QianZhangTuDianChang() {
+        return _35_QianZhangTuDianChang;
+    }
+
+    public void set_35_QianZhangTuDianChang(String _35_QianZhangTuDianChang) {
+        this._35_QianZhangTuDianChang = _35_QianZhangTuDianChang;
+    }
+
+    public String get_36_ZuGongDu() {
+        return _36_ZuGongDu;
+    }
+
+    public void set_36_ZuGongDu(String _36_ZuGongDu) {
+        this._36_ZuGongDu = _36_ZuGongDu;
+    }
+
+    public String get_37_NeiWaiFanDu() {
+        return _37_NeiWaiFanDu;
+    }
+
+    public void set_37_NeiWaiFanDu(String _37_NeiWaiFanDu) {
+        this._37_NeiWaiFanDu = _37_NeiWaiFanDu;
+    }
+
+    public String get_3_FuWei() {
+        return _3_FuWei;
+    }
+
+    public void set_3_FuWei(String _3_FuWei) {
+        this._3_FuWei = _3_FuWei;
+    }
+
+    public String get_4_DouWei() {
+        return _4_DouWei;
+    }
+
+    public void set_4_DouWei(String _4_DouWei) {
+        this._4_DouWei = _4_DouWei;
+    }
+
+    public String get_5_JiaoWanWei() {
+        return _5_JiaoWanWei;
+    }
+
+    public void set_5_JiaoWanWei(String _5_JiaoWanWei) {
+        this._5_JiaoWanWei = _5_JiaoWanWei;
+    }
+
+    public String get_6_JiaoZhiZhou() {
+        return _6_JiaoZhiZhou;
+    }
+
+    public void set_6_JiaoZhiZhou(String _6_JiaoZhiZhou) {
+        this._6_JiaoZhiZhou = _6_JiaoZhiZhou;
+    }
+
+    public String get_7_WaiHuaiXiaGao() {
+        return _7_WaiHuaiXiaGao;
+    }
+
+    public void set_7_WaiHuaiXiaGao(String _7_WaiHuaiXiaGao) {
+        this._7_WaiHuaiXiaGao = _7_WaiHuaiXiaGao;
+    }
+
+    public String get_8_HouGenTuGao() {
+        return _8_HouGenTuGao;
+    }
+
+    public void set_8_HouGenTuGao(String _8_HouGenTuGao) {
+        this._8_HouGenTuGao = _8_HouGenTuGao;
+    }
+
+    public String get_9_ZhouShangGao() {
+        return _9_ZhouShangGao;
+    }
+
+    public void set_9_ZhouShangGao(String _9_ZhouShangGao) {
+        this._9_ZhouShangGao = _9_ZhouShangGao;
+    }
+}
--- app/src/main/java/com/bintutu/shop/ui/DetailActivity.java
+++ app/src/main/java/com/bintutu/shop/ui/DetailActivity.java
@@ -1,10 +1,16 @@
 package com.bintutu.shop.ui;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.bintutu.shop.R;
+import com.bintutu.shop.bean.LeftBean;
+import com.bintutu.shop.bean.RightBean;
+import com.google.gson.Gson;
 
 public class DetailActivity  extends BaseActivity{
+
 
     @Override
     protected void initContentView(Bundle savedInstanceState) {
@@ -13,11 +19,38 @@ public class DetailActivity  extends BaseActivity{
 
     @Override
     protected void init() {
+        getData();
+
+
+
+        findViewById(R.id.detail_image_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
+
 
     }
 
     @Override
     protected void setListener() {
 
+    }
+
+    public void getData() {
+        Gson gson = new Gson();
+
+        String leftJson = "{ \"10_FuWeiGao\" : 0.0, \"11_1ZhiZhiGao\" : 0.0, \"12_DaMoZhiGao\" : 0.0, \"13_JiaoWanGao\" : 0.0, \"14_JiaoZhiKuan\" : 0.0, \"15_ZhiWeiKuan\" : 75.0062, \"16_DiBanKuan\" : 75.0062, \"17_MuZhiLiKuan\" : 9.89464, \"18_XiaoZhiWaiKuan\" : 56.4657, \"19_1ZhizhiLiKuan\" : 17.5741, \"1_FootLen\" : 221.054, \"20_5ZhizhiLiKuan\" : 55.9658, \"21_YaoWoWaiKuan\" : 73.5143, \"22_ZhongXinDiKuan\" : 56.0287, \"23_JiaoHuaiNeiKuan\" : 0.0, \"24_MuZhiWaiTuChang\" : 198.949, \"25_XiaoZhiDuanChang\" : 182.37, \"26_XiaoZhiWaiTuChang\" : 172.422, \"27_1ZhiZhiChang\" : 160.264, \"28_5ZhiZhiChang\" : 140.369, \"29_FuGuChang\" : 121.58, \"2_ZhiWei\" : 0.0, \"30_YaoWoChang\" : 90.6323, \"31_ZhouShangDianChang\" : 85.1059, \"32_WaiHuaiGuZhongChang\" : 49.7372, \"33_ZhongXinChang\" : 39.7898, \"34_HouGenBianChang\" : 8.84217, \"35_QianZhangTuDianChang\" : 152.085, \"36_ZuGongDu\" : 0.0, \"37_NeiWaiFanDu\" : 0.0, \"3_FuWei\" : 0.0, \"4_DouWei\" : 0.0, \"5_JiaoWanWei\" : 0.0, \"6_JiaoZhiZhou\" : 0.0, \"7_WaiHuaiXiaGao\" : 0.0, \"8_HouGenTuGao\" : 0.0, \"9_ZhouShangGao\" : 0.0 }";
+
+        LeftBean leftBean = gson.fromJson(leftJson, LeftBean.class);//将json数据转换成user类实体
+        Log.e("resJson", leftBean.get_15_ZhiWeiKuan() + "......1");
+
+        String rightJson = "{ \"10_FuWeiGao\" : 0.0, \"11_1ZhiZhiGao\" : 0.0, \"12_DaMoZhiGao\" : 0.0, \"13_JiaoWanGao\" : 0.0, \"14_JiaoZhiKuan\" : 0.0, \"15_ZhiWeiKuan\" : 45.1571, \"16_DiBanKuan\" : 45.1571, \"17_MuZhiLiKuan\" : 0.0, \"18_XiaoZhiWaiKuan\" : 0.0, \"19_1ZhizhiLiKuan\" : 0.0, \"1_FootLen\" : 212.314, \"20_5ZhizhiLiKuan\" : 0.0, \"21_YaoWoWaiKuan\" : 0.0, \"22_ZhongXinDiKuan\" : 0.0, \"23_JiaoHuaiNeiKuan\" : 0.0, \"24_MuZhiWaiTuChang\" : 0.0, \"25_XiaoZhiDuanChang\" : 0.0, \"26_XiaoZhiWaiTuChang\" : 0.0, \"27_1ZhiZhiChang\" : 0.0, \"28_5ZhiZhiChang\" : 0.0, \"29_FuGuChang\" : 0.0, \"2_ZhiWei\" : 0.0, \"30_YaoWoChang\" : 87.0485, \"31_ZhouShangDianChang\" : 81.7407, \"32_WaiHuaiGuZhongChang\" : 47.7705, \"33_ZhongXinChang\" : 0.0, \"34_HouGenBianChang\" : 8.49254, \"35_QianZhangTuDianChang\" : 146.072, \"36_ZuGongDu\" : 0.0, \"37_NeiWaiFanDu\" : 0.0, \"3_FuWei\" : 0.0, \"4_DouWei\" : 0.0, \"5_JiaoWanWei\" : 0.0, \"6_JiaoZhiZhou\" : 0.0, \"7_WaiHuaiXiaGao\" : 0.0, \"8_HouGenTuGao\" : 0.0, \"9_ZhouShangGao\" : 0.0 }";
+        RightBean rightBean = gson.fromJson(rightJson, RightBean.class);//将json数据转换成user类实体
+        Log.e("resJson", rightBean.get_15_ZhiWeiKuan() + "......2");
     }
 }
